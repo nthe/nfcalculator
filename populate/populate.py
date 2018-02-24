587f8ea1b57ad6710c6512baba12526ddc681736
jantoth <jantoth@ralos.com>
2018-02-24 12:10:15 +0100

    tables IngredientNutritionFact, Ingredient populated

--- populate/populate.py
+++ populate/populate.py
@@ -1,25 +1,64 @@
 import os
-import pandas as pd
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'initial.settings')
 import django
+import pandas as pd
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nfc.settings")
 django.setup()
-from nfcalculator.models import Ingredient, \
-                                Product, \
-                                ProductIngredient, \
-                                IngredientNutritionFact
+from nfcalculator.models import Ingredient, IngredientNutritionFact
+
 
 def load_products_from_file(file_name=None, sheet_name=None):
+    """
+
+    :param file_name:
+    :param sheet_name:
+    :return: returns loaded dataframe - basically data from excel file :)
+    """
     file_name = file_name
     sheet_name = sheet_name
     xl = pd.ExcelFile(file_name)
     data_frame = xl.parse(sheet_name)
     return data_frame
 
+
+def add_to_ingredient(_name, _description):
+    """
+
+    :param _name:
+    :param _description:
+    :return: returns what has been added to sqlite database
+    """
+    ingredient = Ingredient.objects.get_or_create(
+        name=_name,
+        description=_description
+    )[0]
+    ingredient.save()
+    return ingredient
+
+
+def add_to_ingredientnutritionfact(_ingredient_id,
+                                   _nutrition_fact_id,
+                                   _weight):
+    """
+
+    :param ingredient_id:        for example butter - Liptov with      #5
+    :param nutrition_fact_id:    for example Lipids with fixed         #1
+    :param weight:               Lipds per hundred grams for example   30g
+    :return:
+    """
+    inf = IngredientNutritionFact.objects.get_or_create(
+        ingredient_id=_ingredient_id,
+        nutrition_fact_id=_nutrition_fact_id,
+        weight=_weight
+        )[0]
+    inf.save()
+    return inf
+
+
 file = "data.xlsx"
 sheet = "ingredient"
 cwd = os.getcwd()
 df = load_products_from_file(file, sheet)
-column_names = df.head(1).columns.values.tolist()
+column_names = df.columns.values.tolist()
 
 df = pd.DataFrame(df)
 
@@ -36,26 +75,41 @@ df = pd.DataFrame(df)
 'Z toho cukry'
 'Bielkoviny'
 'Soľ'
+
+table: nutritionfact
+1	Lipids
+2	Saturated
+3	Sacharides
+4	Sugar
+5	Protein
+6	Salt
+
 '''
 
-# for index, row in df.head(1).iterrows():
-#     print(row['Názov produktu'],
-#           row['Výrobca/Distribútor'],
-#           row['Zloženie'],
-#           row['Alergény'],
-#           row['Môže obsahovať'],
-#           row['Tuky'],
-#           row['Z toho nasýtené mastné kyseliny'],
-#           row['Sacharidy'],
-#           row['Z toho cukry'],
-#           row['Bielkoviny'],
-#           row['Soľ'])
+imported_ingredients = {}
+for index, row in df.head(10).iterrows():
+    # inserting to table: Ingredient
+    ingredient_object = add_to_ingredient(row['Názov produktu'], row['Zloženie'])
+
+    Lipids = row['Tuky']
+    Saturated = row['Z toho nasýtené mastné kyseliny']
+    Sacharides = row['Sacharidy']
+    Sugar = row['Z toho cukry']
+    Protein = row['Bielkoviny']
+    Salt = row['Soľ']
+
+    # inserting to table: IngredientNutritionFact
+    #           zip([number identifiers], [actual weights for Lipids, Saturated, ...]):
+    for i, j in zip([1, 2, 3, 4, 5, 6], [Lipids, Saturated, Sacharides, Sugar, Protein, Salt]):
+        ingredientnutritionfact_object = add_to_ingredientnutritionfact(
+            _ingredient_id=ingredient_object.id,
+            _nutrition_fact_id=i,
+            _weight=float(str(j).replace(',', '.'))
+        )
 
 
-def add_to_ingridient():
-    ingridient = Ingredient.objects.get_or_create(top_name=random.choice(topics))[0]
-    ingridient.save()
-    return ingridient
+
+
 
 
 
